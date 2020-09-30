88a9307cfaf2ea0572bf9a243298900d3d4013aa
Константин Спешилов <SpeshilovKV@edu.stu.ru>
2020-09-30 12:17:31 +0700

    Тестовое изменение

--- CardGame/Program.cs
+++ CardGame/Program.cs
@@ -6,7 +6,7 @@ namespace CardGame
     {
         static void Main(string[] args)
         {
-            Console.WriteLine("Hello World!");
+            Console.WriteLine("Hello Worlfgghgh!");
         }
     }
 }
